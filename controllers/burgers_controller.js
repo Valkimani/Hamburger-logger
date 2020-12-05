277245ab178c4908a8c863625e96d8abdb7364ff
Valkimani <66477953+Valkimani@users.noreply.github.com>
2020-12-05 16:00:25 -0500

    Created a views folder and added an index.handlebars.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -20,7 +20,7 @@ router.post("/api/burger", function(req, res) {
 burger.create([
     "name", "devoured"
   ], [
-    req.body.name, req.body.devoured.
+    req.body.name, req.body.devoured
   ], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
