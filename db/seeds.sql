8aae4f6f42141a66c81cecd6dbc48a28dbebba99
Valkimani <66477953+Valkimani@users.noreply.github.com>
2020-12-04 20:54:21 -0500

    Updated the values in the seeds.sql.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO cats (name) VALUES ('Molly');
-INSERT INTO cats (name) VALUES ('Charlie');
-INSERT INTO cats (name, sleepy) VALUES ('Poppy', true);
+INSERT INTO burgers (name) VALUES ('Molly');
+INSERT INTO burgers (name) VALUES ('Charlie');
+INSERT INTO burgers (name, devoured) VALUES ('Poppy', true);
 
