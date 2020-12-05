99eedd89a25707931b0de42f258c23abf97235e1
Valkimani <66477953+Valkimani@users.noreply.github.com>
2020-12-04 20:41:16 -0500

    Added sql querries to the schema file.

---
+++ db/schema.sql
@@ -0,0 +1,20 @@
+### Schema
+
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+-- * Switch to or use the `burgers_db`.
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	burger_name STRING,
+    devoured BOOLEAN,
+	PRIMARY KEY (id)
+);
+
+
+
+
+
+
